0a353da1466f5fd6f37b55ede9b698887f63a242
ridwan.fansuri@dana.id <ridwan.fansuri@dana.id>
2021-09-07 21:50:32 +0700

    finish section_09 add maps

---
+++ maps/src/User.ts
@@ -0,0 +1,30 @@
+import faker from 'faker'
+
+import { Mappable } from './CustomMap'
+
+export class User implements Mappable {
+  name: string
+  location: {
+    lat: number,
+    lng: number,
+  }
+  color: string
+
+
+  constructor() {
+    this.name = faker.name.firstName()
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    }
+    this.name = faker.name.firstName()
+    this.color = 'RED'
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+      <h1>User Name: ${this.name}</h1>
+    </div>`
+  }
+}
